08aaafd29b2d7971779cafce3590344bcff8df4a
Kim SeungHwan <babodoban@gmail.com>
2022-04-10 01:32:59 +0900

    make changing color with const, let, if/else

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 const h1 = document.querySelector(".title");
 
 function handleTitleClick() {
-    if (h1.style.color = "black") {
-        h1.style.color = "blue";
+    const currentColor = h1.style.color;
+    let newColor;
+
+    if (currentColor === "blue") {
+        newColor = "black";
     } else {
-        h1.style.color = "black";
+        newColor = "blue";
     }
+    
+    h1.style.color = newColor;
+
     console.log(h1.style.color);
 }
 
-function handleMouseEnter () {
-    h1.innerText = "mouse is here!";
-}
-
-function handleMouseLeave () {
-    h1.innerText = "mouse is gone";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato"
-}
-
-function handleWidnowCopy() {
-    alert("Catch the Copy actoin!")
-}
-
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWidnowCopy);
